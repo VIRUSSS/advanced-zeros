2c793d22e86e42a12dc12c95daec50294291f12e
virus <virus09@mail.ru>
2018-10-07 23:03:20 +0300

    task advanced zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+    const primeNumber = {};
+    let tmpBase = base;
+    for (let i = 2; i <= tmpBase; i++) {
+        if (tmpBase % i == 0) {
+            let c = 0;
+            while (tmpBase % i == 0) {
+                tmpBase /= i;
+                c++;
+            }
+            primeNumber[i] = c;
+        }
+    }
+
+    const res = [];
+    for (let item in primeNumber) {
+        let tmpNumber = number;
+        let c = 0;
+        while (tmpNumber != 0) {
+            tmpNumber = Math.floor(tmpNumber/item);
+            c += tmpNumber;
+        }
+        res.push(Math.floor(c / primeNumber[item]));
+    }
+    return Math.min.apply(null, res);
 }
